b146325dfa60387f8691d8d44f6f17b22d75c5cb
Mark <markrassamni@me.com>
2017-06-23 02:19:54 -0700

    Video 6

--- WeatherStatus/AppDelegate.swift
+++ WeatherStatus/AppDelegate.swift
@@ -11,14 +11,26 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    //variables
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        statusItem.button?.title = "--°"
+        statusItem.action = #selector(AppDelegate.displayPopUp(_:))
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+    }
+    
+    @objc func displayPopUp(_ sender: AnyObject){
+        let storyboard = NSStoryboard(name: NSStoryboard.Name(rawValue: "Main"), bundle: nil)
+        guard let vc = storyboard.instantiateController(withIdentifier: NSStoryboard.SceneIdentifier(rawValue: "WeatherVC"))
+            as? NSViewController else { return }
+        let popoverView = NSPopover()
+        popoverView.contentViewController = vc
+        popoverView.behavior = .transient
+        popoverView.show(relativeTo: statusItem.button!.bounds, of: statusItem.button!, preferredEdge: .minY)
     }
 
 
--- WeatherStatus/WeatherVC.swift
+++ WeatherStatus/WeatherVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WeatherVC.swift
 //  WeatherStatus
 //
 //  Created by Mark Rassamni on 6/23/17.
@@ -8,12 +8,21 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class WeatherVC: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear() {
+        
+    }
+    
+    override func viewDidAppear() {
+        self.view.layer?.backgroundColor = CGColor(red: 0.29, green: 0.72, blue: 0.98, alpha: 1.0)
+        
     }
 
     override var representedObject: Any? {
